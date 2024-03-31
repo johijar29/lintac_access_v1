6c2c730ffc7a9a49ba5d01a7781fb1c1d6aac5ed
jhijar <jhijar@lintac.cl>
2024-03-30 21:25:31 -0300

    Direccionamiento a mi DIR templates

--- lintac_access/settings.py
+++ lintac_access/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'lintac_access.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Aplicaciones/equipamiento/templates/equipamiento'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
